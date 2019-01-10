bc85e5dab01bda55358381186fe77873c91502fa
lukasz <robson27-r@hotmail.com>
2019-01-10 12:56:43 +0100

    nowy plik

---
+++ src/bibloteka.java
@@ -0,0 +1,24 @@
+public class bibloteka {
+
+    public static void main(String[] args) {
+
+        String title = "W pustyni i w puszczy";
+        String author = "Henryk Sienkiewicz";
+        int releaseDate = 2010;
+        int pages = 296;
+        String publisher = "Greg";
+        String isbn = "92659874563";
+
+        System.out.println("appName");
+        System.out.println("Książki dostępne w biblotece");
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(releaseDate);
+        System.out.println(pages);
+        System.out.println(publisher);
+        System.out.println(isbn);
+
+
+
+    }
+}
